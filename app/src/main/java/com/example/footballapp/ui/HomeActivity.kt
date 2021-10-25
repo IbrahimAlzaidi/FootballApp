1abd108a23a167f522d3cad4289177f319154a04
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-10-26 02:31:52 +0300

    feat: initial commit

---
+++ app/src/main/java/com/example/footballapp/ui/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.footballapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.footballapp.R
+import com.example.footballapp.databinding.ActivityHomeBinding
+import com.example.footballapp.viewModel.HomeActivityViewModel
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private val viewModel = HomeActivityViewModel()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+    }
+}
