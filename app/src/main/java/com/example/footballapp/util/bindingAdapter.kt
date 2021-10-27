1af493012566525751f04085de1c8b4ffaecc147
Nooralden <nooraldenakel@gmail.com>
2021-10-27 10:28:39 +0300

    feat:add Glide fun & view check to bindingAdapter

--- app/src/main/java/com/example/footballapp/util/bindingAdapter.kt
+++ app/src/main/java/com/example/footballapp/util/bindingAdapter.kt
@@ -1,8 +1,32 @@
 package com.example.footballapp.util
 
+import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.footballapp.model.State
 import com.example.footballapp.ui.base.BaseAdapter
+
+@BindingAdapter(value = ["app:whenError"])
+fun <T> showWhenError(view: View, state: State<T>?) =
+    if (state is State.Error)view.visibility = View.VISIBLE
+    else view.visibility = View.GONE
+
+@BindingAdapter(value = ["app:whenLoading"])
+fun <T> showWhenLoading(view: View, state: State<T>?) =
+    if (state is State.Error)view.visibility = View.VISIBLE
+    else view.visibility = View.GONE
+
+@BindingAdapter(value = ["app:whenSuccess"])
+fun <T> showWhenSuccess(view: View, state: State<T>?) =
+    if (state is State.Error)view.visibility = View.VISIBLE
+    else view.visibility = View.GONE
+
+@BindingAdapter(value = ["app:ImageUrl"])
+fun imageUrl(view:ImageView,url:String){
+    Glide.with(view).load(url).into(view)
+}
 
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerView(view: RecyclerView,items: List<T>?){
