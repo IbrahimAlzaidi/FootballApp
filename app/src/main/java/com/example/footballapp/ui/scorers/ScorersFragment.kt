2d6d98037988752424653ce957a3ad24b016fbb7
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-10-28 22:33:27 +0300

    feat: add scorers fragment and view model

---
+++ app/src/main/java/com/example/footballapp/ui/scorers/ScorersFragment.kt
@@ -0,0 +1,13 @@
+package com.example.footballapp.ui.scorers
+
+import com.example.footballapp.R
+import com.example.footballapp.databinding.FragmentScorersBinding
+import com.example.footballapp.ui.base.BaseFragment
+import com.example.footballapp.viewModel.ScorersViewModel
+
+class ScorersFragment: BaseFragment<FragmentScorersBinding>(R.layout.fragment_scorers){
+    override fun setup() {
+        binding.viewModel = viewModel as ScorersViewModel?
+    }
+
+}
