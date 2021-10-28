7fe5bea6d9e30b37d78b88dc020dac26fced5f57
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-10-28 22:32:54 +0300

    feat: add player details fragment and view model

---
+++ app/src/main/java/com/example/footballapp/ui/playerDetails/PlayerDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.footballapp.ui.playerDetails
+
+import com.example.footballapp.R
+import com.example.footballapp.databinding.FragmentPlayerDetailsBinding
+import com.example.footballapp.ui.base.BaseFragment
+import com.example.footballapp.viewModel.PlayerDetailsViewModel
+
+class PlayerDetailsFragment: BaseFragment<FragmentPlayerDetailsBinding>(R.layout.fragment_player_details){
+    override fun setup() {
+        binding.viewModel = viewModel as PlayerDetailsViewModel?
+    }
+
+}
