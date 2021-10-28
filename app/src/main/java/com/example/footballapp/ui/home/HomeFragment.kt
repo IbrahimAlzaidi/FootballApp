fe1c5559f46cfb49872a5eefe0ec065a93d3f4c6
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-10-28 22:34:32 +0300

    fix: refactors and rename class

--- app/src/main/java/com/example/footballapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/footballapp/ui/home/HomeFragment.kt
@@ -1,19 +1,15 @@
 package com.example.footballapp.ui.home
 
-import androidx.fragment.app.viewModels
 import com.example.footballapp.R
 import com.example.footballapp.databinding.FragmentHomeBinding
 import com.example.footballapp.ui.base.BaseFragment
-import com.example.footballapp.viewModel.HomeFragmentViewModel
+import com.example.footballapp.viewModel.HomeViewModel
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
-    private val viewModel : HomeFragmentViewModel by viewModels()
     override fun setup() {
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
+        binding.viewModel = viewModel as HomeViewModel?
         val adapter = TeamsAdapter(mutableListOf(),null)
         binding.recycleView.adapter = adapter
     }
-
 }
