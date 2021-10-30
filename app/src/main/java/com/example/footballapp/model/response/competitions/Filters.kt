49d26946f92093c09fbeacef83624b301229914e
Nooralden <nooraldenakel@gmail.com>
2021-10-30 05:29:51 +0300

    fix: delete old response

--- app/src/main/java/com/example/footballapp/model/response/competitions/Filters.kt
+++
@@ -1,6 +0,0 @@
-package com.example.footballapp.model.response.competitions
-
-
-import com.google.gson.annotations.SerializedName
-
-class Filters
--- app/src/main/java/com/example/footballapp/model/response/standings/Filters.kt
+++
@@ -1,4 +0,0 @@
-package com.example.footballapp.model.response.standings
-
-
-class Filters
