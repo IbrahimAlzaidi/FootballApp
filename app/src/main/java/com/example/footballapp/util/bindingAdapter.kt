f38e1aaaffbd3727283796da2c6c2290dcce32a5
Nooralden <nooraldenakel@gmail.com>
2021-11-02 12:02:21 +0300

    fix:add imageTeam & nameTeam fun to dataBindings

--- app/src/main/java/com/example/footballapp/util/bindingAdapter.kt
+++ app/src/main/java/com/example/footballapp/util/bindingAdapter.kt
@@ -14,21 +14,21 @@ import com.github.twocoffeesoneteam.glidetovectoryou.GlideApp
 
 @BindingAdapter(value = ["app:whenError"])
 fun <T> showWhenError(view: View, state: State<T>?) =
-    if (state is State.Error)view.visibility = View.VISIBLE
+    if (state is State.Error) view.visibility = View.VISIBLE
     else view.visibility = View.GONE
 
 @BindingAdapter(value = ["app:whenLoading"])
 fun <T> showWhenLoading(view: View, state: State<T>?) =
-    if (state is State.Error)view.visibility = View.VISIBLE
+    if (state is State.Error) view.visibility = View.VISIBLE
     else view.visibility = View.GONE
 
 @BindingAdapter(value = ["app:whenSuccess"])
 fun <T> showWhenSuccess(view: View, state: State<T>?) =
-    if (state is State.Error)view.visibility = View.VISIBLE
+    if (state is State.Error) view.visibility = View.VISIBLE
     else view.visibility = View.GONE
 
 @BindingAdapter(value = ["app:ImageUrl"])
-fun imageUrl(view:ImageView,url:String?){
+fun imageUrl(view: ImageView, url: String?) {
     GlideApp.with(view)
         .load(url)
         .placeholder(R.drawable.ic_baseline_cloud_download_24)
@@ -37,17 +37,27 @@ fun imageUrl(view:ImageView,url:String?){
 }
 
 @BindingAdapter(value = ["app:items"])
-fun <T> setRecyclerView(view: RecyclerView,items: List<T>?){
-    if (items != null){
+fun <T> setRecyclerView(view: RecyclerView, items: List<T>?) {
+    if (items != null) {
         (view.adapter as BaseAdapter<T>?)?.setItems(items)
-    }else{
+    } else {
         (view.adapter as BaseAdapter<T>?)?.setItems(emptyList())
     }
 }
 
 @BindingAdapter(value = ["app:goals"])
-fun setGoalsFromStatistics(view:TextView,response : Response?){
+fun setGoalsFromStatistics(view: TextView, response: Response?) {
     view.setText(
-        response?.statistics?.sumOf { it.goals?.total ?: 0  }?.toString()
+        response?.statistics?.sumOf { it.goals?.total ?: 0 }?.toString()
     )
+}
+
+@BindingAdapter(value = ["app:teamLogo"])
+fun setTeamLogo(view: ImageView, response: Response?) {
+    Glide.with(view).load(response?.statistics?.joinToString { it.team?.logo.toString() }).into(view)
+}
+
+@BindingAdapter(value = ["app:teamName"])
+fun setTeamName(view: TextView, response: Response?) {
+    view.setText(response?.statistics?.joinToString { it.team?.name.toString()})
 }
