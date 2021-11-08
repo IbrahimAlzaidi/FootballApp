b1ec63daa280176842ece371a2cd970ccd8c1146
Nooralden <nooraldenakel@gmail.com>
2021-11-08 15:35:35 +0300

    fix : Refactor all rearrange false dimension and size.

--- app/src/main/java/com/example/footballapp/ui/matchDetails/matchState/MatchStateAdapter.kt
+++ app/src/main/java/com/example/footballapp/ui/matchDetails/matchState/MatchStateAdapter.kt
@@ -1,10 +1,9 @@
 package com.example.footballapp.ui.matchDetails.matchState
 
 import com.example.footballapp.R
-import com.example.footballapp.model.response.fixtureAllData.MatchStatisticInformation
 import com.example.footballapp.model.response.matchStatistic.MatchResultData
 import com.example.footballapp.ui.base.BaseAdapter
 
 class MatchStateAdapter(items: List<MatchResultData>) :
-    BaseAdapter<MatchResultData>(items,null, R.layout.match_statictis_items) {
+    BaseAdapter<MatchResultData>(items,null, R.layout.match_statistic_items) {
 }
