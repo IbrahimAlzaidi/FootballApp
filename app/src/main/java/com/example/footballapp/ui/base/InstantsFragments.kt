42f8f87798559aae7ff564c935653d8b7d3014fd
Nooralden <nooraldenakel@gmail.com>
2021-11-15 21:34:19 +0300

    fix / name of InstantsFragments

--- app/src/main/java/com/example/footballapp/ui/base/InstantsFragments.kt
+++ app/src/main/java/com/example/footballapp/ui/base/InstantsFragments.kt
@@ -3,10 +3,10 @@ package com.example.footballapp.ui.base
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 
-open class InstantsFragments<T : Fragment>(private val cls: Class<T>) {
+open class InstantsFragments<T : Fragment>(private val Fragment: Class<T>) {
 
     fun newInstance(vararg args: Pair<String, Any?>): T {
-        return cls.newInstance().apply {
+        return Fragment.newInstance().apply {
             arguments = bundleOf(*args)
         }
     }
